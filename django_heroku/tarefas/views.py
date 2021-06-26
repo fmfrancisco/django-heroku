5addb784713fa1f7709c638c151e2251ac07a36a
Francisco Menezes <franciscofm.menezes@gmail.com>
2021-06-26 11:08:21 -0300

    Comentado todo código das tarefas pendentes

--- django_heroku/tarefas/views.py
+++ django_heroku/tarefas/views.py
@@ -6,15 +6,17 @@ from django_heroku.tarefas.models import Tarefa
 
 # Create your views here.
 def home(request):
-    tarefas_pendentes = Tarefa.objects.filter(feita=False)
+    #tarefas_pendentes = Tarefa.objects.filter(feita=False)
     if request.method == 'POST':
         form = TarefaNovaForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('tarefas:home'))
         else:
-            tarefas_pendentes = Tarefa.objects.filter(feita=False)
-            return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes}, status=400)
+            #tarefas_pendentes = Tarefa.objects.filter(feita=False)
+            #return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes}, status=400)
+            return render(request, 'tarefas/home.html', {'form': form}, status=400)
     
-    tarefas_pendentes = Tarefa.objects.filter(feita=False)
-    return render(request, 'tarefas/home.html', {'tarefas_pendentes': tarefas_pendentes})
+    #tarefas_pendentes = Tarefa.objects.filter(feita=False)
+    #return render(request, 'tarefas/home.html', {'tarefas_pendentes': tarefas_pendentes})
+    return render(request, 'tarefas/home.html')
